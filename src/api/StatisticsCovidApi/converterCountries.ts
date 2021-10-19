8bfc952ac82635c8f0a31dce6dda816cc2ba0067
Vika Mashkovich <vika.mashkovich@gmail.com>
2021-10-19 16:49:30 +0300

    changed statisticsProvider

--- src/api/StatisticsCovidApi/converterCountries.ts
+++ src/api/StatisticsCovidApi/converterCountries.ts
@@ -1,19 +1,19 @@
 import { Country } from '../../common/entities/countryConstructor';
-// import { IConverterFlags } from '../../common/types/flagsTypes';
+import { IConverterFlags } from '../../common/types/flagsTypes';
 import { ICountryStatistics } from '../../common/types/statisticsTypes';
-// import Flags from '../MOCK/flagsData/flags.json';
+import Flags from '../MOCK/flagsData/flags.json';
 
 export const converterCountries = (countries: any[]):ICountryStatistics[] => (
   countries.reduce((acc, country) => {
-    // const flags = Flags as Array<IConverterFlags>;
-    // const flagCountry:IConverterFlags | undefined = flags.find((item:IConverterFlags) => (
-    //   item.CountryCode === country.CountryCode
-    // ));
-    // let flagIcon:string = '';
+    const flags = Flags as Array<IConverterFlags>;
+    const flagCountry:IConverterFlags | undefined = flags.find((item:IConverterFlags) => (
+      item.CountryCode === country.CountryCode
+    ));
+    let flagIcon:string = '';
 
-    // if (flagCountry) {
-    //   flagIcon = flagCountry.flagBase64;
-    // }
+    if (flagCountry) {
+      flagIcon = flagCountry.flagBase64;
+    }
 
     acc.push(new Country({
       id: country.ID,
@@ -24,7 +24,7 @@ export const converterCountries = (countries: any[]):ICountryStatistics[] => (
       newDeaths: country.NewDeaths,
       totalDeaths: country.TotalDeaths,
       totalRecovered: country.TotalConfirmed - country.TotalDeaths,
-      flag: 'flagIcon',
+      flag: flagIcon,
     }));
 
     return acc;
